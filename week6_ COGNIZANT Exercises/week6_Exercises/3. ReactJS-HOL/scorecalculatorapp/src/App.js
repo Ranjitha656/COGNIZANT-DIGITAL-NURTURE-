7824812a3e24526dedbd6e7fc9ac8f0b771a39c9
Ranjitha656 <vtu22397@veltech.edu.in>
2025-07-27 22:17:16 +0530

    week 6

---
+++ week6_ COGNIZANT Exercises/week6_Exercises/3. ReactJS-HOL/scorecalculatorapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore name="T Ranjitha" school="B.R EM High School" total={599} goal={6} Percentage = "99.9%" />
+    </div>
+  );
+}
+
+export default App;
---
+++ week6_ COGNIZANT Exercises/week6_Exercises/6. ReactJS-HOL/trainersapp/src/TrainersMock.js
@@ -0,0 +1,9 @@
+import Trainer from "./Trainer";
+
+const trainers = [
+  new Trainer(1, "Ranjitha", "ranjitha@example.com", "1234578987", "React", ["JS", "Redux"]),
+  new Trainer(2, "Angel", "angel@example.com", "9876543516", "Angular", ["TS", "RxJS"]),
+  new Trainer(3, "Priya", "priya@example.com", "3435556755", "Vue", ["Vuex", "Composition API"])
+];
+
+export default trainers;
